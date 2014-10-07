3b24664ce4462fc0a78ee43ff6c6780c186ba984
josiah14 <praenato14@gmail.com>
2014-10-07 00:51:06 -0500

    add facebook icon, minor refactoring

--- Elm.elm
+++ Elm.elm
@@ -2,36 +2,54 @@ import Maybe
 import Text
 import Window
 
-main = content <~ Window.dimensions
+main = banner <~ Window.dimensions
 
 linkStyle : Int -> Int -> Text.Style
-linkStyle w h = { typeface = ["Tahoma"]
-                , height = Just ((sqrt <| (toFloat h ^ 2.0) + (toFloat w ^ 2.0)) / 35.0)
+linkStyle w h = { typeface = ["century gothic", "sans-serif"]
+                , height = Just <| percentOfDiag (w,h) 0.05
                 , color = white
-                , bold = True
-                , italic = False
+                , bold = False
+                , italic = True
                 , line = Nothing
                 }
 
-banner : (Int, Int) -> Element
-banner (w, h) = opacity 0.5 <| color black <| container w (ceiling (( sqrt <| (toFloat h ^ 2.0) + (toFloat w ^ 2.0)) / 15)) midTop <| plainText ""
+bannerBg : (Int, Int) -> Element
+bannerBg (w, h) = opacity 0.5 <| color black <| container w (ceiling <| percentOfDiag (w,h) 0.065) midTop <| plainText ""
 
 bannerText : (Int, Int) -> Element
-bannerText (w, h) = flow down
-                      [ container w (ceiling (( sqrt <| (toFloat h ^ 2.0) + (toFloat w ^ 2.0)) / 33.0)) midTop (toText "Joanna Berkebile"
-                          |> Text.style (linkStyle w h)
-                          |> centered)
-                      , container w (ceiling (( sqrt <| (toFloat h ^ 2.0) + (toFloat w ^ 2.0)) / 25.0)) middle (toText "Professional live singer and voice instructor"
-                          |> Text.color white
-                          |> Text.height ((sqrt <| (toFloat h ^ 2.0) + (toFloat w ^ 2.0)) / 45.0)
-                          |> centered)
-                      ]
+bannerText (w, h) =
+  flow right
+    [ container (floor <| toFloat w * 0.85) (ceiling <| percentOfDiag (w, h) 0.060) midLeft
+        (toText " Joanna Berkebile"
+          |> Text.style (linkStyle w h)
+          |> leftAligned)
+    , facebookIcon (w,h)
+    ]
 
-content : (Int, Int) -> Element
-content (w, h) =
+facebookIcon : (Int, Int) -> Element
+facebookIcon (w, h) =
+  container (floor <| toFloat w * 0.14) (ceiling <| percentOfDiag (w, h) 0.060) midRight
+  <| link "https://www.facebook.com/joanna.carey2"
+  <| fittedImage (floor <| percentOfDiag (w,h) 0.055) (floor <| percentOfDiag (w,h) 0.055) "./resources/facebook.png"
+
+banner : (Int, Int) -> Element
+banner (w, h) =
   layers
     [ opacity 1.0 <| fittedImage w h "./resources/jo-0.jpg"
-    , banner (w, h)
+    , bannerBg (w, h)
     , bannerText (w, h)
     ]
+
+{-- --------------------------------------------------------------------------------------------------------------------
+ -- Utility functions
+ ----------------------------------------------------------------------------------------------------------------------}
+
+-- This uses the pythagorean theorem to calculate the the size based on the width/height dimensions and the percentage
+-- passed in.  The first parameter is the container dimensions (treated as the width and height of a rectangle) and the
+-- second parameter is the percentage of the calculated diagonal to return.
+percentOfDiag : (Int, Int) -> Float -> Float
+percentOfDiag (winWidth, winHeight) percentage =
+  let w = toFloat winWidth
+      h = toFloat winHeight
+  in (*) percentage << sqrt <| (w ^ 2.0) + (h ^ 2.0)
 
